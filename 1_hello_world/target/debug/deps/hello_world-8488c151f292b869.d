2403e751156f953f1c59c30c73155eec4d9bcd68
jomarusoup <sigojy000@gmail.com>
2024-04-26 13:57:34 +0900

    new

---
+++ 1_hello_world/target/debug/deps/hello_world-8488c151f292b869.d
@@ -0,0 +1,5 @@
+/home/cho/github/rust_lang/hello_world/target/debug/deps/hello_world-8488c151f292b869: src/main.rs
+
+/home/cho/github/rust_lang/hello_world/target/debug/deps/hello_world-8488c151f292b869.d: src/main.rs
+
+src/main.rs:
---
+++ 1_hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/cho/github/rust_lang/hello_world/target/debug/hello_world: /home/cho/github/rust_lang/hello_world/src/main.rs
